2a3054f213dba9fbd86d409a89853085df330d32
sevaivanov <m.seva.ivanov@gmail.com>
2014-10-13 18:07:28 -0400

    Partial video population

--- scripts/database/videos.py
+++ scripts/database/videos.py
@@ -1,40 +1,101 @@
-from frontend.models import Video
+from datetime import date
+from frontend.models import User, Video
 
+KEDFILMS_FOUNDER = User.objects.get(nick = "kedfilms-founder")
+
+"""
+Delete all records
+"""
+Video.objects.all().delete()
+
+"""
+I Intro
+"""
 video = Video(
-	filename = "camera_intro.mov",
-	title = "Camera assembly",
-	category = "I"
+	staticfilepath = "vid/intro/camera_intro.ogv",
+	title = "Preparations",
+	director = "Seva Ivanov",
+	description = "",
+	category = Video.IN,
+    hardware = "Camera: Canon EOS REBEL T3i",
+	application = "SFX: Adobe After Effect",
+	date_created = date(2012, 07, 17),
+	owner = KEDFILMS_FOUNDER
 )
 video.save()
 video = Video(
-	filename = "industrial.mov",
-	title = "Industrial",
-	category = "I"
+	staticfilepath = "vid/intro/industrial.ogv",
+	title = "Montréal",
+	director = "Seva Ivanov",
+	description = "Unique picture animation",
+	category = Video.IN,
+    hardware = "Camera: Canon EOS REBEL T3i",
+	application = "SFX: Adobe After Effect",
+	date_created = date(2012, 04, 22),
+	owner = KEDFILMS_FOUNDER
 )
 video.save()
 video = Video(
-	filename = "qc_timelapse.mov",
-	title = "Quebec city morning timelapse",
-	category = "I"
+	staticfilepath = "vid/intro/qc_timelapse.ogv",
+	title = "Quebec city",
+	director = "Seva Ivanov",
+	description = "Timelapse",
+	category = Video.IN,
+    hardware = "Camera: Canon EOS REBEL T3i",
+	application = "SFX: Adobe After Effect",
+	date_created = date(2012, 12, 23),
+	owner = KEDFILMS_FOUNDER
 )
 video.save()
 
+"""
+C complete
+"""
 video = Video(
-	filename = "flying_car.mov",
+	staticfilepath = "vid/complete/flying_car.ogv",
 	title = "Back in the forest",
-	category = "C"
+	director = "Seva Ivanov",
+	description = "",
+	category = Video.CM,
+    hardware = "Camera: Canon EOS REBEL T3i",
+	application = "SFX: Adobe After Effect",
+	date_created = date(2011, 10, 12),
+	owner = KEDFILMS_FOUNDER
 )
 video.save()
 video = Video(
-	filename = "gta5.mov",
+	staticfilepath = "vid/complete/gta5.ogv",
 	title = "Gta V - Away from the keyboard",
-	category = "C"
+	director = "Seva Ivanov",
+	description = "Three hours of improvisation.",
+	category = Video.CM,
+    hardware = "Camera: Canon EOS REBEL T3i",
+	application = "SFX: Adobe After Effect",
+	date_created = date(2011, 11, 02),
+	owner = KEDFILMS_FOUNDER
 )
 video.save()
 video = Video(
-	filename = "spring_recap.mov" ,
-	title = "Just relax - Spring Recap",
-	category = "C"
+	staticfilepath = "vid/complete/spring_recap.mov" ,
+	title = "Spring",
+	director = "Seva Ivanov",
+	description = "Pictures in motion",
+	category = Video.CM,
+    hardware = "Camera: Canon EOS REBEL T3i",
+	application = "SFX: Adobe After Effect",
+	date_created = date(2012, 06, 24),
+	owner = KEDFILMS_FOUNDER
 )
 video.save()
-
+video = Video(
+	staticfilepath = "vid/complete/daveland.ogv" ,
+	title = "Daveland",
+	director = "Seva Ivanov",
+	description = "High school project",
+	category = Video.CM,
+    hardware = "Camera: Canon EOS REBEL T3i",
+	application = "SFX: Adobe After Effect",
+	date_created = date(2011, 05, 15),
+	owner = KEDFILMS_FOUNDER
+)
+video.save()

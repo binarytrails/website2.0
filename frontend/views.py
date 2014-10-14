c48337a6ab082711e713eca97ee6194e4a9e2f2b
sevaivanov <m.seva.ivanov@gmail.com>
2014-10-14 01:59:22 -0400

    render_to_response -> render

--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,5 @@
 import os
-from django.shortcuts import HttpResponse, render_to_response
+from django.shortcuts import HttpResponse, render
 from .models import User, Skill, Photo, Video
 from kedfilms import utils
 
@@ -11,12 +11,11 @@ def home(request):
     for item in Skill.objects.all().filter(owner='kedfilms-founder').order_by('category').values('category').distinct():
         skills_categories.append(item['category'])
 
-    return render_to_response(
-        "frontend/sections/home.html",{
-            "skills_categories": skills_categories,
-            "skills": Skill.objects.all().filter(owner='kedfilms-founder')
-        }
-    )
+    return render(request, "frontend/sections/home.html",
+    {
+        "skills_categories": skills_categories,
+        "skills": Skill.objects.all().filter(owner='kedfilms-founder')
+    })
 
 def articles(request):
     try:
@@ -30,26 +29,24 @@ def articles(request):
     except ValueError:
         template = "frontend/sections/articles.html"    
 
-    return render_to_response(
-        template, {
-            "title": "Articles",
-            "subtitle": "Subtitle"
-        }
-    )
+    return render(request, template,
+    {
+        "title": "Articles",
+        "subtitle": "Subtitle"
+    })
 
 # categories: ((GN, General), (PF, Portfolio))
 def photos(request):
     if os.path.exists(IMG_DIR):
-        return render_to_response(
-            "frontend/sections/photos.html",{
-                "title": "Photography",
-                "subtitle": "Capture the moment in time",
-                "portfolio_title": "Porfolio",
-                "portfolio_images": Photo.objects.all().filter(category = Photo.PF),
-                "general_title": "General",
-                "general_images": Photo.objects.all().filter(category = Photo.GN)
-        }
-    )
+        return render(request, "frontend/sections/photos.html",
+        {
+            "title": "Photography",
+            "subtitle": "Capture the moment in time",
+            "portfolio_title": "Porfolio",
+            "portfolio_images": Photo.objects.all().filter(category = Photo.PF),
+            "general_title": "General",
+            "general_images": Photo.objects.all().filter(category = Photo.GN)
+        })
 
 def photos_slideshow(request, category=None):
     photos = None
@@ -69,23 +66,23 @@ def photos_slideshow(request, category=None):
     photos = Photo.objects.all().filter(category = category)
 
     if photos:
-        return render_to_response(
-            "frontend/sections/slideshow.html",{
-                "previous_location": previous_location,
-                "photos": photos
+        return render(request, "frontend/sections/slideshow.html",
+        {
+            "previous_location": previous_location,
+            "photos": photos
         })
 
 # categories: ((IN, Intro), (CM, Complete))
 def videos(request):
     if os.path.exists(VID_DIR):
-        return render_to_response(
-            "frontend/sections/videos.html",{
-                "title": "Short Films",
-                "intro_title": "Brief introductory passage",
-                "intro_videos": Video.objects.all().filter(category = Video.IN),
-                "complete_title": "The affair is over, ended, finished",
-                "complete_videos": Video.objects.all().filter(category = Video.CM),
-                "unofficial_title": "An incomplete flower",
-                "unofficial_videos": None
-        }
-    )
+        return render(request, "frontend/sections/videos.html",
+        {
+            "title": "Short Films",
+            "intro_title": "Brief introductory passage",
+            "intro_videos": Video.objects.all().filter(category = Video.IN),
+            "complete_title": "The affair is over, ended, finished",
+            "complete_videos": Video.objects.all().filter(category = Video.CM),
+            "unofficial_title": "An incomplete flower",
+            "unofficial_videos": Video.objects.all().filter(category = Video.UN)
+        })
+

ba9d0cc780cf78d7c8d6562122ed359565455841
sevaivanov <m.seva.ivanov@gmail.com>
2014-10-16 04:05:36 -0400

    Videos posters will give the previews

--- reset-database.sh
+++ reset-database.sh
@@ -18,17 +18,13 @@ echo; echo "Creating database with South wrapper"; echo
 ./manage.py migrate
 ./manage.py shell < scripts/database/users.py
 ./manage.py shell < scripts/database/skills.py
+./manage.py shell < scripts/database/videos.py
 
 echo; echo "Should we create a moq for photos mapped in 'scripts/database/photos.py'? (yes, no):"; read moqphotos
 if [ "$moqphotos" == "yes" ]; then
     ./manage.py shell < scripts/database/photos-moq.py
 else
     ./manage.py shell < scripts/database/photos.py
-fi
-
-echo; echo "Do you have original videos mapped in 'scripts/database/videos.py'? (yes, no):"; read hasvideos
-if [ "$hasvideos" == "yes" ]; then
-    ./manage.py shell < scripts/database/videos.py
 fi
 
 echo; echo "Everything is done! Launching the server..."; echo

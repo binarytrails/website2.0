51719c0e6935363574749d02d308c4f84a1385c8
sevaivanov <sevaivanov@kedfilms.com>
2014-12-02 19:00:08 -0500

    ! longer (20->30) filenames in urls

--- kedfilms/urls.py
+++ kedfilms/urls.py
@@ -8,12 +8,12 @@ urlpatterns = patterns('',
     url(r'^home/$', 'frontend.views.home', name='home'),
 
     url(r'^articles/$', 'frontend.views.articles', name='articles'),
-    url(r'^artcile/(?P<category>[\w\-]{1,10})/(?P<article>[\w\-]{1,20})$', 'frontend.views.article', name='article'),
+    url(r'^artcile/(?P<category>[\w\-]{1,10})/(?P<article>[\w\-]{1,30})$', 'frontend.views.article', name='article'),
 
     url(r'^photos/$', 'frontend.views.photos', name='photos'),
     url(r'^photos/gallery/(?P<category>[\w]{1,10})/$', 'frontend.views.gallery', name='gallery'),
     url(r'^photos/slideshow/(?P<category>[\w]{1,10})/$', 'frontend.views.slideshow', name='slideshow'),
-    url(r'^photos/mslideshow/(?P<category>[\w]{1,10})/(?P<image>[\w\-._]{1,20})/$', 'frontend.views.mslideshow', name='mslideshow'),
+    url(r'^photos/mslideshow/(?P<category>[\w]{1,10})/(?P<image>[\w\-._]{1,30})/$', 'frontend.views.mslideshow', name='mslideshow'),
 
     url(r'^videos/$', 'frontend.views.videos', name='videos'),
 )

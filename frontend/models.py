62f7c71e02a6274bcad10551766a8afe75833e61
sevaivanov <sevaivanov@kedfilms.com>
2015-03-04 01:48:01 -0500

    + Photo model methods for PhotoAdmin

--- frontend/models.py
+++ frontend/models.py
@@ -1,8 +1,4 @@
-"""
-* unique constraints are used because multiple primary keys aren't supported.
-"""
-
-# Copyright (C) 2014 Vsevolod Ivanov
+# Copyright (C) 2015 Vsevolod Ivanov
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,38 +13,61 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-from django.db import models
-from datetime import date
-from django.contrib import admin
+"""
+    unique constraints are used because multiple primary keys aren't supported.
+"""
 
-class User(models.Model):
-    nick = models.CharField(
-        primary_key = True,
-        max_length = 50
-    )
-    name = models.CharField(
-        max_length = 50,
-        blank = False
-    )
-    lastname = models.CharField(
-        max_length = 50,
-        blank = False
-    )
-    email = models.EmailField(
-        blank = False
-    )
+import os, shutil, pyexiv2
+from datetime import date
+
+from django.db import models
+from django.contrib import admin
+from django.conf import settings
+from django.core.exceptions import ValidationError
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+MEDIA_URL = settings.MEDIA_URL
+IMAGES_ROOT = os.path.join(settings.MEDIA_ROOT, "images")
+IMAGES_DIRNAME = "original"
+THUMBS_DIRNAMES = ['x200', 'x800']
+
+# offline uploads only, hence, no media/ used.
+def get_photo_upload_to_by_category(instance, filename):
+    return os.path.join("images", instance.category, "original", filename)
 
 class Photo(models.Model):
-    GN = 'general'
-    PF = 'portfolio'
- 
-    filename = models.CharField(
-        primary_key = True,
-        max_length = 250
+    def __unicode__(self):
+        return self.title
+
+    # Add new categories here
+    GENERAL = 'general'
+    PORTFOLIO = 'portfolio'
+    CATEGORIES = (
+        (GENERAL, 'General'),
+        (PORTFOLIO, 'Portfolio'),
+    )
+
+    category = models.CharField(
+        max_length = 10,
+        blank = False,
+        choices = CATEGORIES,
+        default = GENERAL
+    )
+    cached_category = models.CharField(
+        max_length = 200,
+        blank = True
+    )
+    image = models.ImageField(
+        blank = False,
+        upload_to = get_photo_upload_to_by_category
+    )
+    cached_image_path = models.CharField(
+        max_length = 200,
+        unique = True,
+        blank = True
     )
     fragment_identifier = models.CharField(
-        max_length = 20,
-        unique = True
+        max_length = 20
     )
     title = models.CharField(
         max_length = 50,
@@ -58,28 +77,99 @@ class Photo(models.Model):
         max_length = 50,
         blank = False
     )
-    category = models.CharField(
-        max_length = 2,
-        blank = False,
-        default = GN
-    )
     hardware = models.CharField(
-        max_length = 50
+        max_length = 50,
+        blank = True
     )
     application = models.CharField(
-        max_length = 50
+        max_length = 50,
+        blank = True
     )
     date_created = models.DateField(
         default = date.today,
         blank = False
-    )    
-    owner = models.ForeignKey(
-        'User'
     )
+
+    def get_image_url(self):
+        return os.path.join(MEDIA_URL, "images", self.category, IMAGES_DIRNAME, 
+            os.path.basename(str(self.image))
+        )
+
+    def get_image_abspath(self):
+        return os.path.join(IMAGES_ROOT, self.category, IMAGES_DIRNAME, 
+            os.path.basename(str(self.image))
+        )
+
+    def get_thumbnails_abspaths(self):
+        filename = os.path.basename(self.cached_image_path)
+        thumbnails_abspaths = []
+
+        for thumb_dirname in THUMBS_DIRNAMES:
+            thumbnail_path = os.path.join(IMAGES_ROOT, self.category, thumb_dirname, filename)
+            thumbnails_abspaths.append(thumbnail_path)
+
+        return thumbnails_abspaths
+
+    def generate_thumbnails(self):
+        filename = os.path.basename(self.cached_image_path)
+
+        for thumb_dirname in THUMBS_DIRNAMES:
+            thumbnail_path = os.path.join(IMAGES_ROOT, self.category, thumb_dirname, filename)
+
+            # ImageMagick; New thumb size is also its directory name
+            os.system("convert " + self.cached_image_path + " -resize " + 
+                thumb_dirname + " " + thumbnail_path
+            )
+
+    def generate_image_xmp_metadata(self):
+        metadata = pyexiv2.ImageMetadata(self.cached_image_path)
+        metadata.read()
+
+        metadata["Xmp.xmp.title"] = self.title
+        metadata["Xmp.xmp.fragment_identifier"] = self.fragment_identifier
+        metadata["Xmp.xmp.author"] = self.author
+        metadata["Xmp.xmp.date_created"] = str(self.date_created)
+        metadata["Xmp.xmp.application"] = self.application
+        metadata["Xmp.xmp.hardware"] = self.hardware
+
+        metadata.write()
+
+    def move_image_to_updated_category(self):
+        filename = os.path.basename(self.cached_image_path)
+        subdirectories = THUMBS_DIRNAMES + [IMAGES_DIRNAME]
+
+        for subdirectory in subdirectories:
+            current_image_path = os.path.join(IMAGES_ROOT, self.cached_category, subdirectory, filename)
+            new_image_path = os.path.join(IMAGES_ROOT, self.category, subdirectory, filename)
+
+            # destination has this file
+            if os.path.isfile(new_image_path):
+                raise ValidationError("Can't change the image category: Same image filename exists.")
+
+            shutil.move(current_image_path, new_image_path)
+
+    def delete_image(self):
+        if os.path.exists(self.cached_image_path):
+            os.remove(self.cached_image_path)
+
+    def delete_thumbnails(self):
+        filename = os.path.basename(self.cached_image_path)
+
+        for thumb_dirname in THUMBS_DIRNAMES:
+            thumbnail_path = os.path.join(IMAGES_ROOT, self.category, thumb_dirname, filename)
+
+            if os.path.exists(thumbnail_path):
+                os.remove(thumbnail_path)
+
+    # overwritten method
+    def delete(self, *args, **kwargs):
+        super(Photo, self).delete(*args, **kwargs)
+        self.delete_image()
+        self.delete_thumbnails()
 
     class Meta:
         unique_together = (
-            ('owner', 'category', 'title')
+            ('category', 'fragment_identifier')
         )
 
 class Video(models.Model):
@@ -120,13 +210,10 @@ class Video(models.Model):
         default = date.today,
         blank = False
     )
-    owner = models.ForeignKey(
-        'User'
-    )
 
     class Meta:
         unique_together = (
-            ('owner', 'category', 'title')
+            ('category', 'title')
         )
 
 class Skill(models.Model):
@@ -159,12 +246,8 @@ class Skill(models.Model):
         max_digits = 1,
         decimal_places = 0
     )
-    owner = models.ForeignKey(
-        'User'
-    )
     
     class Meta:
         unique_together = (
-            ('owner' , 'category', 'title')
+            ('category', 'title')
         )
-

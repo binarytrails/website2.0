1a5af871db8d5b19f344d98b4d43e136bdb6ce37
sevaivanov <sevaivanov@kedfilms.com>
2015-09-25 15:15:11 -0400

    + old-browser: choice of browsers suggestions

--- frontend/views.py
+++ frontend/views.py
@@ -64,7 +64,8 @@ def render_no_mobile_version(request):
 def detect_old_browsers(initial_view):
     def wrapped_view(request, *args, **kwargs):
         if any(agent in request.META['HTTP_USER_AGENT'].lower() for agent in IE_USERAGENT_TAGS):
-            return render(request, "frontend/errors/old-browser.html")
+            browsers_suggestion = {"firefox": True, "chrome": True, "safari": True}
+            return render(request, "frontend/errors/old-browser.html", browsers_suggestion)
 
         return initial_view(request, *args, **kwargs)
     return wrapped_view

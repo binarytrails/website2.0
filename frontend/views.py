1f3b17df961224a67e6937c9c2112584ad77d496
sevaivanov <sevaivanov@kedfilms.com>
2015-12-15 02:14:15 -0500

    ! frontend general refactoring: + themes support + binding hardcoded locations to global variables in views

--- frontend/views.py
+++ frontend/views.py
@@ -2,8 +2,8 @@
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# the Free Software Foundation, either platform 3 of the License, or
+# (at your option) any later platform.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -13,202 +13,226 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-import os, time
-
-from django.shortcuts import HttpResponse, render, redirect
-from django.core.urlresolvers import reverse
-from django.views.decorators.cache import never_cache
-
-from .models import Photo, Video, Project
-from kedfilms import utils
-from kedfilms.decorators import old_browsers
-import json
+import os
 
 from django.conf import settings
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.cache import never_cache
 
-PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-STATIC_ROOT = settings.STATIC_ROOT
-MEDIA_URL = settings.MEDIA_URL
+from .models import Project, Photo, Video
 
-STATIC_PROJECTS = os.path.join(STATIC_ROOT, "projects")
-STATIC_FRONTEND = os.path.join(STATIC_ROOT, "frontend")
-MEDIA_IMAGES = os.path.join(settings.MEDIA_ROOT, "images")
+from kedfilms import utils
+
+APP = "frontend"
+MEDIA = settings.MEDIA_URL
+STATIC = os.path.join(settings.STATIC_ROOT, APP)
+ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+
+THEME = os.path.join(APP, "themes/bits")
+ERRORS = os.path.join(APP, "themes/generic")
 
 MOBILE_HOSTS = ["m.kedfilms.com", "m.sevaivanov.com"]
 
-BASE_TEMPLATE = "base.html"
+def is_mobile(request):
+    # .mobile -> minidetector.Middleware
+    return request.mobile or request.get_host() in MOBILE_HOSTS
+
+def template_exists(template):
+    return os.path.exists(os.path.join(APP, "templates", template))
+
+def template_prefix(request):
+    return ("-mobile.html" if is_mobile(request) else ".html")
+
+def merge_context(request, new_context=None):
+    base_context = {
+        "APP": APP,
+        "THEME": THEME,
+        "PLATFORM": "mobile" if is_mobile(request) else "desktop",
+        "PARENT": os.path.join(THEME, "base" + template_prefix(request))
+    }
+    if new_context: return dict(base_context.items() + new_context.items())
+    return base_context
+
+# decorators
+
+IE_USERAGENT_TAGS = ["msie", "trident"]
+
+def old_browsers(initial_view):
+
+    def wrapped_view(request, *args, **kwargs):
+        ie_useragent = any(agent in request.META["HTTP_USER_AGENT"].lower() 
+            for agent in IE_USERAGENT_TAGS)
+
+        if ie_useragent:
+            template = os.path.join(ERRORS, "old-browser.html")
+            if template_exists(template) == False: return error404(request)
+
+            return render(request, template, merge_context(request, {     
+                "PARENT": os.path.join(ERRORS, "base.html"),
+                "THEME": ERRORS,
+                "SUPPORTED_BROWSERS": {"Firefox" ,"Chrome", "Safari"}
+            }))
+
+        return initial_view(request, *args, **kwargs)
+
+    return wrapped_view
+
+# views
+
+@never_cache
+@old_browsers
+def home(request):
+    template = os.path.join(THEME, "home.html")
+    if template_exists(template) == False: return error404(request)
+    
+    return render(request, template, merge_context(request))
+
+@never_cache
+@old_browsers
+def articles(request):
+    template = os.path.join(THEME, "articles.html")
+    if template_exists(template) == False: return error404(request)
+    
+    return render(request, template, merge_context(request))
+
+@never_cache
+@old_browsers
+def article(request, category=None, article=None):
+    if not article or not category: return error404(request)
+
+    article_path = os.path.join(STATIC, "md/", category, article + ".md")
+    if os.path.isfile(article_path) == False: return error404(request)
+
+    template = os.path.join(THEME, "article.html")
+    if template_exists(template) == False: return error404(request)
+    
+    return render(request, template, merge_context(request, {
+        "html": utils.markdownToHtml(os.path.join(article_path))
+    }))
+
+@never_cache
+@old_browsers
+def projects(request):
+    template = os.path.join(THEME, "projects.html")
+    if template_exists(template) == False: return error404(request)
+
+    return render(request, template, merge_context(request, {
+        "start_year": 2015,
+        "passed_years": range(3),
+        "months": range(12, 0, -1),
+        "projects": Project.TIMELINE
+    }))
+
+@never_cache
+@old_browsers
+def photos(request):
+    template = os.path.join(THEME, "photos" + template_prefix(request))
+    if template_exists(template) == False: return error404(request)
+
+    return render(request, template, merge_context(request, {
+        "categories": Photo.CATEGORIES
+    }))
+
+@never_cache
+@old_browsers
+def gallery(request, category):
+    categories = Photo.objects.all().values_list("category").distinct()
+    has_category = any(item[0] == category for item in categories)
+    if not has_category: return error404(request)
+
+    context = merge_context(request, {
+        "category": category,
+        "photos": Photo.objects.all().filter(
+            category = category).order_by("-date_created")
+    })
+
+    if is_mobile(request) == False:
+        context = dict(context.items() + {
+            "last": Photo().get_previous_category(category),
+            "next": Photo().get_next_category(category)
+        }.items())
+
+    template = os.path.join(THEME, "photos-gallery" + template_prefix(request))
+    if template_exists(template) == False: return error404(request)
+
+    return render(request, template, context)
+
+@never_cache
+@old_browsers
+def slideshow(request, category=None, fragment_id=None):
+    categories = Photo.objects.all().values_list("category").distinct()
+    has_category = any(item[0] == category for item in categories)
+    if not has_category: return error404(request)
+
+    context = None
+    if is_mobile(request):
+        image = Photo.objects.get(fragment_identifier = fragment_id)
+        if os.path.isfile(image.get_image_abspath()) == False:
+            return error404(request)
+
+        context = {"image_url": image.get_image_url()}
+    else:
+        context = {
+            "category": category,
+            "photos": Photo.objects.all().filter(
+                category = category).order_by("-date_created")
+        }
+
+    template = os.path.join(THEME, "photos-slideshow" + template_prefix(request))
+    if template_exists(template) == False: return error404(request)
+
+    return render(request, template, merge_context(request, context)) 
+
+@never_cache
+@old_browsers
+def videos(request):
+    template = os.path.join(THEME, "videos" + template_prefix(request))
+    if template_exists(template) == False: return error404(request)
+
+    return render(request, template, merge_context(request, {
+        "posters_url": os.path.join(STATIC, "img/video-poster/"),
+        "videos_url": os.path.join(STATIC, "vid/"),
+        "categories": Video.CATEGORIES,
+        "videos": Video.objects.all().filter().order_by("-date_created")
+    }))
+
+# errors
 
 def error404(request):
-    return render(request, "frontend/errors/generic-simple-text.html",
-    {
+    template = os.path.join(ERRORS, "simple-text.html")
+    if template_exists(template) == False: return HttpResponse("404 Not Found")
+
+    return render(request, template, merge_context(request, {
+        "PARENT": os.path.join(ERRORS, "base.html"),
+        "THEME": ERRORS,
         "header": "404 NOT FOUND",
         "subtitle": """
             Sorry, the page you requested was not found.
         """
-    })
+    }))
 
-# .mobile -> minidetector.Middleware
-def is_mobile(request): return request.mobile or request.get_host() in MOBILE_HOSTS
+def no_mobile(request):
+    template = os.path.join(ERRORS, "simple-text.html")
+    if template_exists(template) == False: return error404(request)
 
-def get_app_version(request):
-    if is_mobile(request): return "mobile"
-    else: return "desktop"
-
-def render_no_mobile_version(request):
-    return render(request, "frontend/errors/generic-simple-text.html",
-    {
-        "header": "The mobile version is not available.",
+    return render(request, template, merge_context(request, {
+        "PARENT": os.path.join(ERRORS, "base.html"),
+        "THEME": ERRORS,
+        "header": "The mobile platform is not available.",
         "body": """
             Don"t worry, there is an easy fix.
             All you have to do is to consult our website using 
             your favorite computer to enjoy the experience to its fullest.
         """
-    })
+    }))
 
-@never_cache
-@old_browsers
-def home(request):
-    version = get_app_version(request)
-    parent = os.path.join("frontend", version, BASE_TEMPLATE)
+def fullscreen_image(request, image_source):
+    template = os.path.join(ERRORS, "fullscreen-image.html")
+    if template_exists(template) == False: return error404(request)
 
-    return render(request, "frontend/generic/home.html", {
-        "version": version,
-        "parent": parent
-    })
-
-@never_cache
-@old_browsers
-def projects(request):
-    version = get_app_version(request)
-    template = "frontend/generic/projects.html"
-    parent = os.path.join("frontend", version, BASE_TEMPLATE)
-
-    return render(request, template, {
-        "parent": parent,
-        "version": version,
-        "start_year": 2015,
-        "passed_years": range(3),
-        "months": range(12, 0, -1),
-        "projects": Project.TIMELINE
-    })
-
-@never_cache
-@old_browsers
-def articles(request):
-    template = "frontend/generic/articles.html"
-    parent = os.path.join("frontend", get_app_version(request), BASE_TEMPLATE)
-    return render(request, template, { "parent": parent })
-
-@never_cache
-@old_browsers
-def article(request, category=None, article=None):
-    if not article or not category:
-        return error404(request)
-
-    article += ".md"
-    if os.path.isfile(os.path.join(STATIC_FRONTEND, "md/", category, article)) == False:
-        return error404(request)
-
-    template = "frontend/generic/article.html"
-    parent = os.path.join("frontend", get_app_version(request), BASE_TEMPLATE)
-    html = utils.markdownToHtml(os.path.join(STATIC_FRONTEND, "md/", category, article))
- 
-    return render(request, template, { "parent": parent, "html": html })
-
-@never_cache
-@old_browsers
-def photos(request):
-    categories = Photo.CATEGORIES
-    
-    if is_mobile(request):
-        template = "frontend/mobile/photos.html"
-
-        # omit the "internet in motion" aka the #4 section.
-        # categories = [tuple(x for x in y if x)
-        #     for y in categories if y != categories[4]
-        # ]
-
-    else:
-        template = "frontend/desktop/photos.html"
-    
-    parent = os.path.join("frontend", get_app_version(request), BASE_TEMPLATE)
-
-    return render(request, template,
-    {
-        "parent": parent,
-        "categories": categories
-    })
-
-@never_cache
-@old_browsers
-def gallery(request, category):
-    if not os.path.exists(MEDIA_IMAGES): return Http404
-
-    unique_categories = Photo.objects.all().values_list("category").distinct()
-    if not unique_categories: return error404(request)
-
-    if not any(unique_category[0] == category for unique_category in unique_categories):
-        return error404(request)
-
-    parent = os.path.join("frontend", get_app_version(request), BASE_TEMPLATE)
-
-    if is_mobile(request):
-        return render(request, "frontend/mobile/photos-gallery.html",
-        {
-            "parent": parent,
-            "category": category,
-            "photos": Photo.objects.all().filter(
-                category = category).order_by("-date_created")
-        })
-    else:
-        photo = Photo()
-        return render(request, "frontend/desktop/photos-gallery.html",
-        {
-            "parent": parent,
-            "category": category,
-            "images": Photo.objects.all().filter(
-                category = category).order_by("-date_created"),
-            "last": photo.get_previous_category(category),
-            "next": photo.get_next_category(category)
-        })
-
-@never_cache
-@old_browsers
-def slideshow(request, category=None, fragment_id=None):
-    if not any(unique_category["category"] == category
-        for unique_category in Photo.objects.all().values("category").distinct()
-    ):
-        return error404(request)
-
-    if is_mobile(request):
-        image = Photo.objects.get(fragment_identifier = fragment_id)
-
-        if os.path.isfile(image.get_image_abspath()) == False:
-            return error404(request)
-
-        return render(request, "frontend/mobile/photos-slideshow.html",
-        {
-            "image_url": image.get_image_url()
-        })
-
-    else:
-        return render(request, "frontend/desktop/photos-slideshow.html",
-        {
-            "category": category,
-            "photos": Photo.objects.all().filter(
-                category = category).order_by("-date_created")
-        })
-
-@never_cache
-@old_browsers
-def videos(request):
-    parent = os.path.join("frontend", get_app_version(request), BASE_TEMPLATE)
-
-    return render(request, "frontend/desktop/videos.html",
-    {
-        "parent": parent,
-        "posters_src": "img/video-poster/",
-        "videos_src": "vid/",
-        "categories": Video.CATEGORIES,
-        "videos": Video.objects.all().filter().order_by("-date_created")
-    })
+    return render(request, template, merge_context(request, {
+        "PARENT": os.path.join(ERRORS, "base.html"),
+        "THEME": ERRORS,
+        "image_source": image_source
+    }))

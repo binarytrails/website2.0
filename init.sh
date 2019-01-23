8635c4e98595a69b5bbfaacfcce080ac925edb9b
Seva <seva@binarytrails.net>
2019-01-23 12:46:15 -0500

    ! update init script

--- init.sh
+++ init.sh
@@ -1,4 +1,7 @@
+git submodule init
+git submodule update
 ./manage.py makemigrations
 ./manage.py migrate
 ./manage.py createsuperuser
-./manager.py runserver
+./manage.py collectstatic
+./manage.py runserver
